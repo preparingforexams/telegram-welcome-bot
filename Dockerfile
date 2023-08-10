99651d908a57cd0db33ea6f5bf618f36bcf92441
Björn Petersen <git@bjoernpetersen.net>
2023-08-10 19:45:35 +0200

    Ditch serverless

---
+++ Dockerfile
@@ -0,0 +1,23 @@
+FROM python:3.11-slim
+
+RUN useradd --system --create-home --home-dir /app -s /bin/bash app
+USER app
+ENV PATH=$PATH:/app/.local/bin
+
+WORKDIR /app
+
+RUN pip install pipx==1.2.0 --user --no-cache
+RUN pipx install poetry==1.5.1
+RUN poetry config virtualenvs.create false
+
+COPY [ "poetry.toml", "poetry.lock", "pyproject.toml", "./" ]
+
+# We don't want the tests
+COPY src/app ./src/app
+
+RUN poetry install --only=main
+
+ARG APP_VERSION
+ENV APP_VERSION=$APP_VERSION
+
+ENTRYPOINT [ "poetry", "run", "python", "-m", "app" ]
---
+++ src/app/bot.py
@@ -0,0 +1,60 @@
+import os
+import random
+from typing import Any
+
+from telegram import Update
+from telegram.ext import Application, ChatMemberHandler
+
+_FILE_IDS = [
+    # Saufen
+    "CQACAgIAAxkDAAMGYAQ3hb7J8740ND6tbnwf9m7o7s8AAooKAAIVSiBITKyTWpm8keweBA",
+    # Alkpause
+    "CQACAgIAAxkDAAMHYAQ3qNopch3GK9ZgfFckciIikHAAAosKAAIVSiBICtvI62oH3_seBA",
+    # Trinken
+    "CQACAgIAAxkDAAMIYAQ-CYlzFAABOG5HIV4Z52M2pOpsAAKYCgACFUogSOiJTDCf_HlXHgQ",
+    # Aktuelle Situation
+    "CQACAgIAAxkDAAMnYBGy7xpSrNxeVGqU-hKkmvKVQVoAAlcLAAKJO4hIuBttZLzEECYeBA",
+    # Komasaufen
+    "CQACAgIAAxkBAANZYeaJDEyC6yYa1Qe8RYxfyawvb_IAAmoWAAItbjBLXvJA_jHnjIkjBA",
+]
+
+
+class Bot:
+    def __init__(self) -> None:
+        telegram_token = os.getenv("TELEGRAM_TOKEN")
+        if not telegram_token:
+            raise ValueError("TELEGRAM_TOKEN is missing")
+
+        self.telegram_token = telegram_token
+
+    def run(self) -> None:
+        app = Application.builder().token(self.telegram_token).build()
+        app.add_handler(
+            ChatMemberHandler(
+                self._handle_chat_member,
+                ChatMemberHandler.CHAT_MEMBER,
+            )
+        )
+        app.run_polling(read_timeout=5, allowed_updates=[Update.CHAT_MEMBER])
+
+    async def _handle_chat_member(self, update: Update, _: Any) -> None:
+        chat_member = update.chat_member
+        if chat_member is None:
+            return
+
+        new_member = chat_member.new_chat_member
+        if new_member is None:
+            return
+
+        file_id = random.choice(_FILE_IDS)
+        user = new_member.user
+        username = user.first_name
+        if user["id"] == 1365395775:
+            username = "Katerine"
+
+        await update.get_bot().send_audio(
+            chat_id=chat_member.chat.id,
+            audio=file_id,
+            allow_sending_without_reply=True,
+            caption=f"Willkommen, {username}",
+        )
